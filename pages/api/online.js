630c0bd56ea20d4a0f0ae6bab0cf5ee04748ebf5
SylaHubAccount <erterdterxtwerferqq@gmail.com>
2026-01-22 16:51:48 +0100

    Update online.js

--- pages/api/online.js
+++ pages/api/online.js
@@ -1,1 +1,13 @@
+import users from "./store"
 
+export default function handler(req, res) {
+  const now = Date.now()
+  // Remove users inactive for 10s
+  for (const id in users) {
+    if (now - users[id].lastSeen > 10000) {
+      delete users[id]
+    }
+  }
+
+  res.status(200).json(users)
+}
