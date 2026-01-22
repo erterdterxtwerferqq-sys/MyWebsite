91ae74c5eac1e66c4635582c13d5f042a4208e74
SylaHubAccount <erterdterxtwerferqq@gmail.com>
2026-01-22 16:43:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-const defaultUsername = 'ghost';
-const defaultPassword = '1234';
-
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const errorMessage = document.getElementById('errorMessage');
-    const loginContainer = document.querySelector('.login-container');
-    const successMessage = document.getElementById('successMessage');
-
-    if (username === defaultUsername && password === defaultPassword) {
-        errorMessage.textContent = '';
-        loginContainer.style.display = 'none';
-        successMessage.style.display = 'block';
-    } else {
-        errorMessage.textContent = 'Invalid username or password';
-    }
-});
+fetch("/api/online")
+.then(res => res.json())
+.then(data => {
+  const c = document.getElementById("users")
+  c.innerHTML = ""
+  Object.values(data).forEach(u => {
+    c.innerHTML += `
+      <div class="user">
+        <b>${u.username}</b><br>
+        ${u.displayName}<br>
+        <span class="online">ONLINE</span>
+      </div>
+    `
+  })
+})
